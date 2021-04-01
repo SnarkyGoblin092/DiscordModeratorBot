33006de83030834c5194bf153215d18e3b6a4042
Madar Ádám <vpleft@gmail.com>
2021-04-01 03:41:12 +0000

    Code totally refactored and improved; Using bot instead of Client; Removed features.py; Added nick, ping, insult commands; Added universal send_warning() function; Added new dependencies;

--- features.py
+++
@@ -1,8 +0,0 @@
-import commands
-
-
-async def call_function(channel, author, message, command, level):
-  if command == ".nick":
-    await commands.change_nickname(channel, message, author)
-  elif command == ".print" or command == ".say":
-    await commands.bot_say(channel, message, command)
