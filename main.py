366266d03b4e132b2966693a8b0fbe13b6f32b66
Orsós Emil István <orsosemil092@gmail.com>
2021-04-01 00:19:21 +0000

    Simplified and fixed "send_use_warning"

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def check_commands(message):
 
 
 # sends a warning when a command was used in the wrong channel
-async def send_wrong_channel_warning(channel, author, role, message):
+async def send_wrong_channel_warning(channel, author, message, role):
   embed_message = ""
   if role.name == "DJ":
     embed_message = discord.Embed(title="Figyelmeztetés!", description=":no_entry_sign:   **Rossz csatorna!**\n\nA {0.mention} joghoz tartozó parancsokat csak a {1.mention}-ban használhatod!".format(role, channel), color = 0xff0000)
@@ -31,16 +31,16 @@ async def send_wrong_channel_warning(channel, author, role, message):
 
 
 # sends a warning when a command is used without permission
-async def send_permission_warning(channel, author, message, command, level):
-  embed_message = discord.Embed(title="Figyelmeztetés!", description=":no_entry_sign:   **Nincs jogod ehhez a parancshoz:** {0}\nSzükséges jog: {1}".format("`{}`".format(command), level.mention), color = 0xff0000)
+async def send_permission_warning(channel, author, message, role, command):
+  embed_message = discord.Embed(title="Figyelmeztetés!", description=":no_entry_sign:   **Nincs jogod ehhez a parancshoz:** {0}\nSzükséges jog: {1}".format("`{}`".format(command), role.mention), color = 0xff0000)
   embed_message.set_author(name = author.display_name, icon_url=author.avatar_url)
   await message.delete()
   await channel.send(embed=embed_message)
 
 
 # sends a warning that includes the usage of the used command when the command syntax is incorrect
-async def send_use_warning(channel, author, message, command, role):
-  usage = commands_data[role][command]
+async def send_use_warning(channel, author, message, role, command):
+  usage = commands_data[role.name][command]
   title = "Figyelmeztetés!"
   description = ":x:   **Helytelen használat!**\n\n{0}".format(usage)
   embed_msg = discord.Embed(title=title, description=description, color=0xff9f21)
@@ -79,18 +79,21 @@ async def on_message(message):
   if command:
     role = discord.utils.get(message.guild.roles, name=role_name)
     if not role in msg_author.roles:
-      await send_permission_warning(msg_channel, msg_author, message, command, role)
+      await send_permission_warning(msg_channel, msg_author, message, role, command)
       return
 
     dj_commands = commands_data["DJ"].keys()
     if command in dj_commands and not msg_channel.name == "music":
-      await send_wrong_channel_warning(music_channel, msg_author, role, message)
+      await send_wrong_channel_warning(music_channel, msg_author, message, role)
     elif msg_channel.name == "commands" or msg_channel.name == "bot-test":
+      if not msg.split(command, 1)[1]:
+        await send_use_warning(msg_channel, msg_author, message, role, command)
+        return
       log_msg = "{0.mention} used `{1}`".format(msg_author, msg)
       await bot_log_channel.send(log_msg)
       await features.call_function(msg_channel, msg_author, msg, command, role)
     else:
-      await send_wrong_channel_warning(commands_channel, msg_author, role, message)
+      await send_wrong_channel_warning(commands_channel, msg_author, message, role)
 
 
 if __name__ == "__main__":
