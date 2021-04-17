9ae167c61ee8268154f251eaec1d90b6cd5fcba8
snarkygoblin092 <orsosemil092@gmail.com>
2021-04-17 08:52:03 +0200

    Changes to indentation in code and dealwatch send embeds now *changed indentation size from 2 to 4 *dealwatch sends formatted embeds instead of plain text

--- forum_scraper.py
+++ forum_scraper.py
@@ -4,75 +4,75 @@ import time
 from bs4 import BeautifulSoup
 
 class NetworkErrorDuringScraping(Exception):
-  'Raised when a network error happen during scraping.'
+    'Raised when a network error happen during scraping.'
 
 
 class ForumMessage():
-  def __init__(self, id:int, datetime:str, author_nick:str, text:str):
-    self.id = id
-    self.datetime = datetime
-    self.author_nick = author_nick
-    self.text = text
+    def __init__(self, id:int, datetime:str, author_nick:str, text:str):
+        self.id = id
+        self.datetime = datetime
+        self.author_nick = author_nick
+        self.text = text
 
-    # Check if the text got deals in it
-    text_lower = text.lower()
-    self.has_deals = ('#akció' in text_lower) or ('#akcio' in text_lower)
+        # Check if the text got deals in it
+        text_lower = text.lower()
+        self.has_deals = ('#akció' in text_lower) or ('#akcio' in text_lower)
 
 
 def convert_children_link_tags_to_discord_links(parent):
-  r"Converts the tag's children \<a\> tags to discord embed link format."
-  for a in parent.find_all('a', href=True):
-    href = a['href']
-    text = a.get_text(strip=True)
+    r"Converts the tag's children \<a\> tags to discord embed link format."
+    for a in parent.find_all('a', href=True):
+        href = a['href']
+        text = a.get_text(strip=True)
 
-    dc_link = f'[{text}]({href})'
-    a.replace_with(dc_link) 
+        dc_link = f'[{text}]({href})'
+        a.replace_with(dc_link) 
 
 
 async def request_webpage_src(from_message_id:int) -> str:
-  'Requests the webpage from the given message id. Returns a string of the webpage source it gets.'
-  topic_url = f'https://prohardver.hu/tema/bestbuy_topik_akcio_ajanlasakor_akcio_hashtag_kote/hsz_{from_message_id}-{from_message_id+199}.html'
-  async with aiohttp.ClientSession() as session:
-    async with session.get(topic_url) as response:
-      if not response.status == 200:
-        raise NetworkErrorDuringScraping
+    'Requests the webpage from the given message id. Returns a string of the webpage source it gets.'
+    topic_url = f'https://prohardver.hu/tema/bestbuy_topik_akcio_ajanlasakor_akcio_hashtag_kote/hsz_{from_message_id}-{from_message_id+199}.html'
+    async with aiohttp.ClientSession() as session:
+        async with session.get(topic_url) as response:
+            if not response.status == 200:
+                raise NetworkErrorDuringScraping
 
-      return await response.text()
+            return await response.text()
 
 
 async def scrape(from_message_id):
-  webpage_src = await request_webpage_src(from_message_id=from_message_id)
-  bs = BeautifulSoup(webpage_src, 'html.parser')
+    webpage_src = await request_webpage_src(from_message_id=from_message_id)
+    bs = BeautifulSoup(webpage_src, 'html.parser')
 
-  page_center      = bs.find(name='div', id='center')
-  message_id_tags  = page_center.select('div.msg-list:not(.thread-content) div.card div.card-header span.msg-head-author > span.msg-num a')
-  date_tags        = page_center.select('div.msg-list:not(.thread-content) div.card div.card-header time')
-  author_nick_tags = page_center.select('div.msg-list:not(.thread-content) div.card div.card-body > div.msg-user p.user-title')
-  text_tags        = page_center.select('div.msg-list:not(.thread-content) div.card div.card-body > div.media-body > div.msg-content')
+    page_center = bs.find(name='div', id='center')
+    message_id_tags = page_center.select('div.msg-list:not(.thread-content) div.card div.card-header span.msg-head-author > span.msg-num a')
+    date_tags = page_center.select('div.msg-list:not(.thread-content) div.card div.card-header time')
+    author_nick_tags = page_center.select('div.msg-list:not(.thread-content) div.card div.card-body > div.msg-user p.user-title')
+    text_tags = page_center.select('div.msg-list:not(.thread-content) div.card div.card-body > div.media-body > div.msg-content')
 
-  data_scraped = []
-  for message_id_tag, date_tag, author_nick_tag, text_tag in zip(message_id_tags, date_tags, author_nick_tags, text_tags):
-    id          = int(message_id_tag.get_text(strip=True)[1:]) # Slice out the '#' character
-    datetime    = date_tag.get_text(strip=True)
-    author_nick = author_nick_tag.get_text(strip=True)
+    data_scraped = []
+    for message_id_tag, date_tag, author_nick_tag, text_tag in zip(message_id_tags, date_tags, author_nick_tags, text_tags):
+        id = int(message_id_tag.get_text(strip=True)[1:]) # Slice out the '#' character
+        datetime = date_tag.get_text(strip=True)
+        author_nick = author_nick_tag.get_text(strip=True)
 
-    convert_children_link_tags_to_discord_links(text_tag)
-    text = text_tag.get_text(separator='\n', strip=True)
+        convert_children_link_tags_to_discord_links(text_tag)
+        text = text_tag.get_text(separator='\n', strip=True)
 
-    data_scraped.append(ForumMessage(id, datetime, author_nick, text))
+        data_scraped.append(ForumMessage(id, datetime, author_nick, text))
 
-  return tuple(data_scraped)
+    return tuple(data_scraped)
 
 
 async def scrape_recursively(from_message_id):
-  data = await scrape(from_message_id=from_message_id)
-  if data:
-    last_entry_id = data[-1].id
-    # If we scraped the maximum data of the request (199 entries)
-    if from_message_id+199 == last_entry_id:
-      # then request the next messages recursively.
-      return tuple(data + await scrape_recursively(last_entry_id+1))
-  
-  # else just return the data we scraped
-  return tuple(data)
-  
+    data = await scrape(from_message_id=from_message_id)
+    if data:
+        last_entry_id = data[-1].id
+        # If we scraped the maximum data of the request (199 entries)
+        if from_message_id+199 == last_entry_id:
+            # then request the next messages recursively.
+            return tuple(data + await scrape_recursively(last_entry_id+1))
+    
+    # else just return the data we scraped
+    return tuple(data)
+    
--- main.py
+++ main.py
@@ -15,229 +15,227 @@ bot = commands.Bot(command_prefix='$', help_command=PrettyHelp(), description=de
 
 
 class WrongChannelError(commands.CommandError):
-  'Raised when the user wants to run a command in the channel of music bot.'
-  pass
+    'Raised when the user wants to run a command in the channel of music bot.'
+    pass
 
 class InsultAPIError(Exception):
-  'Raised when the Evil Insult API having issues.'
-  pass
+    'Raised when the Evil Insult API having issues.'
+    pass
 
 
 async def send_warning(reply_to, title, sub_title, description, delete_after=None):
-  'Sends a custom warning by replying to a message.'
-  formatted_description = (
-    f':no_entry_sign:\t**{sub_title}**\n\n{description}'
-  )
-  warning = discord.Embed(title=title, description=formatted_description, color=0xff0000)
-  return await reply_to.reply(embed=warning, delete_after=delete_after)
+    'Sends a custom warning by replying to a message.'
+    formatted_description = (
+        f':no_entry_sign:\t**{sub_title}**\n\n{description}'
+    )
+    warning = discord.Embed(title=title, description=formatted_description, color=0xff0000)
+    return await reply_to.reply(embed=warning, delete_after=delete_after)
 
 
 async def send_command_usage(reply_to, command, delete_after=None):
-  'Sends a command\'s usage by replying to a message.'
-  names = '|'.join([command.name] + command.aliases)
-  signature = command.signature
-  usage = f'```{bot.command_prefix}[{names}] {signature}```'
+    'Sends a command\'s usage by replying to a message.'
+    names = '|'.join([command.name] + command.aliases)
+    signature = command.signature
+    usage = f'```{bot.command_prefix}[{names}] {signature}```'
 
-  embed = discord.Embed(title=command.name)
-  embed.add_field(name='Usage', value=usage, inline=True)
-  return await reply_to.reply(embed=embed, delete_after=delete_after)
+    embed = discord.Embed(title=command.name)
+    embed.add_field(name='Usage', value=usage, inline=True)
+    return await reply_to.reply(embed=embed, delete_after=delete_after)
 
 
 async def get_random_insult():
-  'Gets a random insult asynchronously using the EvilInsult API'
-  async with aiohttp.ClientSession() as session:
-    async with session.get('https://evilinsult.com/generate_insult.php') as response:
-      if not response.status == 200:
-        raise InsultAPIError
+    'Gets a random insult asynchronously using the EvilInsult API'
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://evilinsult.com/generate_insult.php') as response:
+            if not response.status == 200:
+                raise InsultAPIError
 
-      return await response.text()
+            return await response.text()
 
 
 @tasks.loop(minutes=1)
 async def task_dealwatch():
-  try:
-    with open('forum_scraper_last_message_id.txt', 'r') as f:
-      last_message_id = int(f.readline())
-  except (FileNotFoundError, ValueError):
-    with open('forum_scraper_last_message_id.txt', 'w') as f:
-      last_message_id = 84239 # A high default value
-      f.write(str(last_message_id))
-  
-  try:
-    messages = await forum_scraper.scrape_recursively(from_message_id=last_message_id+1)
-  except forum_scraper.NetworkErrorDuringScraping:
-    print('Network error occured during web scraping. task_dealwatch is skipping one iteration now...')
-    return
+    try:
+        with open('forum_scraper_last_message_id.txt', 'r') as f:
+            last_message_id = int(f.readline())
+    except (FileNotFoundError, ValueError):
+        with open('forum_scraper_last_message_id.txt', 'w') as f:
+            last_message_id = 84239 # A high default value
+            f.write(str(last_message_id))
+    
+    try:
+        messages = await forum_scraper.scrape_recursively(from_message_id=last_message_id+1)
+    except forum_scraper.NetworkErrorDuringScraping:
+        print('Network error occured during web scraping. task_dealwatch is skipping one iteration now...')
+        return
 
-  if messages:
-    deals_only_messages = filter(lambda m: m.has_deals, messages)
-    for message in deals_only_messages:
-      msg = (
-        f'#{message.id} {message.author_nick}\n'
-        f'{message.text}\n'
-      )
-      await ch_dealwatch.send(msg)
+    if messages:
+        deals_only_messages = filter(lambda m: m.has_deals, messages)
+        for message in deals_only_messages:
+            embed = discord.Embed(title=message.author_nick)
+            embed.add_field(name=message.datetime, value=message.text)
+            await ch_dealwatch.send(embed=embed)
 
-    last_message_id = messages[-1].id
-    with open('forum_scraper_last_message_id.txt', 'w') as f:
-      f.write(str(last_message_id))
+        last_message_id = messages[-1].id
+        with open('forum_scraper_last_message_id.txt', 'w') as f:
+            f.write(str(last_message_id))
 
 
 @bot.event
 async def on_ready():
-  global music_bot
-  global ch_music
-  global ch_log
-  global ch_commands
-  global ch_bot_test
-  global ch_dealwatch
+    global music_bot
+    global ch_music
+    global ch_log
+    global ch_commands
+    global ch_bot_test
+    global ch_dealwatch
 
-  music_bot = bot.get_user(235088799074484224)
-  ch_music = bot.get_channel(821946547767345152)
-  ch_log = bot.get_channel(827374735830286347)
-  ch_commands = bot.get_channel(827374609234001970)
-  ch_bot_test = bot.get_channel(827374679928471592)
-  ch_dealwatch = bot.get_channel(832438420672086066)
+    music_bot = bot.get_user(235088799074484224)
+    ch_music = bot.get_channel(821946547767345152)
+    ch_log = bot.get_channel(827374735830286347)
+    ch_commands = bot.get_channel(827374609234001970)
+    ch_bot_test = bot.get_channel(827374679928471592)
+    ch_dealwatch = bot.get_channel(832438420672086066)
 
-  task_dealwatch.start()
+    task_dealwatch.start()
 
-  print('Logged in as', bot.user, flush=True)
-  await ch_log.send(f'{bot.user.mention} is online! :vulcan:')
+    print('Logged in as', bot.user, flush=True)
+    await ch_log.send(f'{bot.user.mention} is online! :vulcan:')
 
 
 @bot.listen()
 async def on_message(message):
-  if not message.content.startswith(music_commands.command_prefix) or message.author.bot:
-    return
+    if not message.content.startswith(music_commands.command_prefix) or message.author.bot:
+        return
 
-  is_music_command = message.content.startswith(music_commands.commands)
-  if is_music_command and not message.channel is ch_music:
-    title = 'Warning!'
-    sub_title = 'Wrong channel!'
-    description = f'You can only use {music_bot.mention}\'s command in {ch_music.mention}.'
-    await send_warning(reply_to=message, title=title, sub_title=sub_title, description=description, delete_after=10)
-    await message.delete(delay=10)
+    is_music_command = message.content.startswith(music_commands.commands)
+    if is_music_command and not message.channel is ch_music:
+        title = 'Warning!'
+        sub_title = 'Wrong channel!'
+        description = f'You can only use {music_bot.mention}\'s command in {ch_music.mention}.'
+        await send_warning(reply_to=message, title=title, sub_title=sub_title, description=description, delete_after=10)
+        await message.delete(delay=10)
 
 
 @bot.check
 async def check_channel(ctx):
-  if ctx.channel is ch_music:
-    raise WrongChannelError
+    if ctx.channel is ch_music:
+        raise WrongChannelError
 
-  return True
+    return True
 
 
 @bot.event
 async def on_command_error(ctx, exception):
-  if isinstance(exception, commands.CommandNotFound):
-    title = 'Unknown command!'
-    description = 'The given command could not be found.'
-    await send_warning(reply_to=ctx.message, title='Notice!', sub_title=title, description=description, delete_after=10)
+    if isinstance(exception, commands.CommandNotFound):
+        title = 'Unknown command!'
+        description = 'The given command could not be found.'
+        await send_warning(reply_to=ctx.message, title='Notice!', sub_title=title, description=description, delete_after=10)
 
-  elif isinstance(exception, commands.BadArgument):
-    title = 'Error!'
-    sub_title = 'Bad argument!'
-    description = 'Command got a bad argument.'
-    await send_warning(reply_to=ctx.message, title=title, sub_title=sub_title, description=description, delete_after=10)
-    await send_command_usage(reply_to=ctx.message, command=ctx.command, delete_after=10)
+    elif isinstance(exception, commands.BadArgument):
+        title = 'Error!'
+        sub_title = 'Bad argument!'
+        description = 'Command got a bad argument.'
+        await send_warning(reply_to=ctx.message, title=title, sub_title=sub_title, description=description, delete_after=10)
+        await send_command_usage(reply_to=ctx.message, command=ctx.command, delete_after=10)
 
-  elif isinstance(exception, WrongChannelError):
-    title = 'Warning!'
-    sub_title = 'Prohibited!'
-    description = f'You can only use {music_bot.mention}\'s commands in this channel.'
-    await send_warning(reply_to=ctx.message, title=title, sub_title=sub_title, description=description, delete_after=10)
+    elif isinstance(exception, WrongChannelError):
+        title = 'Warning!'
+        sub_title = 'Prohibited!'
+        description = f'You can only use {music_bot.mention}\'s commands in this channel.'
+        await send_warning(reply_to=ctx.message, title=title, sub_title=sub_title, description=description, delete_after=10)
 
-  else:
-    title = 'Error!'
-    sub_title = 'Unknown error!'
-    description = f'An unknown command error occured!\nException: {exception}'
-    await send_warning(reply_to=ctx.message, title=title, sub_title=sub_title, description=description, delete_after=10)
-  
-  await ctx.message.delete(delay=10)
+    else:
+        title = 'Error!'
+        sub_title = 'Unknown error!'
+        description = f'An unknown command error occured!\nException: {exception}'
+        await send_warning(reply_to=ctx.message, title=title, sub_title=sub_title, description=description, delete_after=10)
+    
+    await ctx.message.delete(delay=10)
 
 
 @bot.listen()
 async def on_command_completion(ctx):
-  user = ctx.author
-  channel = ctx.channel
-  cmd_msg = ctx.message
-  log_msg = f'{user.mention} used a command in {channel.mention} \n```{cmd_msg.clean_content}```'
+    user = ctx.author
+    channel = ctx.channel
+    cmd_msg = ctx.message
+    log_msg = f'{user.mention} used a command in {channel.mention} \n```{cmd_msg.clean_content}```'
 
-  embed = discord.Embed(description=log_msg)
-  await ch_log.send(embed=embed, allowed_mentions=discord.AllowedMentions.none())
+    embed = discord.Embed(description=log_msg)
+    await ch_log.send(embed=embed, allowed_mentions=discord.AllowedMentions.none())
 
 
 @bot.command(name='ping')
 async def _ping(ctx):
-  'Replies with "Pong!".'
-  await ctx.message.reply('Pong!')
+    'Replies with "Pong!".'
+    await ctx.message.reply('Pong!')
 
 
 @bot.command(name='nick', aliases=['nickname', 'name'])
 async def _nick(ctx, name : str = None):
-  'Changes your name to whatever you want. No spaces allowed! Don\'t give any parameters to reset your name.'
-  await ctx.author.edit(nick=name)
+    'Changes your name to whatever you want. No spaces allowed! Don\'t give any parameters to reset your name.'
+    await ctx.author.edit(nick=name)
 
-  if name:
-    msg = 'Nickname set!'
-  else:
-    msg = 'Nickname cleared!'
-  await ctx.reply(msg)
+    if name:
+        msg = 'Nickname set!'
+    else:
+        msg = 'Nickname cleared!'
+    await ctx.reply(msg)
 
 
 @bot.command(name='insult', aliases=['bother', 'curse'])
 async def _insult(ctx, target_user : discord.User = None):
-  'Insults you or somebody else with a random insult.'
+    'Insults you or somebody else with a random insult.'
 
-  if not target_user:
-    target_user = ctx.author
+    if not target_user:
+        target_user = ctx.author
 
-  try:
-    # Denote the time consuming request by 'typing' to the channel
-    async with ctx.typing():
-      random_insult = await get_random_insult()
-    msg = f'{target_user.mention} {random_insult}'
-    await ctx.reply(msg)
+    try:
+        # Denote the time consuming request by 'typing' to the channel
+        async with ctx.typing():
+            random_insult = await get_random_insult()
+        msg = f'{target_user.mention} {random_insult}'
+        await ctx.reply(msg)
 
-  except InsultAPIError:
-    title='Error!'
-    sub_title='Insult API error!'
-    description='Seems like the insult API having issues right now! Try again later.'
-    await send_warning(reply_to=ctx.message, title=title, sub_title=sub_title, description=description, delete_after=10)
-    await ctx.message.delete(delay=10)
+    except InsultAPIError:
+        title='Error!'
+        sub_title='Insult API error!'
+        description='Seems like the insult API having issues right now! Try again later.'
+        await send_warning(reply_to=ctx.message, title=title, sub_title=sub_title, description=description, delete_after=10)
+        await ctx.message.delete(delay=10)
 
 
 @bot.command(name='pin')
 async def _pin(ctx):
-  'Pins the last message of the channel that\'s not a command nor sent by the bot.'
-  history=ctx.channel.history(limit=10)
-  predicate = lambda msg: not msg.content.startswith(ctx.prefix) and not msg.author.bot
-  last_user_made_message = await history.find(predicate)
+    'Pins the last message of the channel that\'s not a command nor sent by the bot.'
+    history=ctx.channel.history(limit=10)
+    predicate = lambda msg: not msg.content.startswith(ctx.prefix) and not msg.author.bot
+    last_user_made_message = await history.find(predicate)
 
-  if last_user_made_message:
-    await last_user_made_message.pin()
-  else:
-    title = 'Notice!'
-    description = 'Couldn\'t find any pinnable messages in the last 10 entries.'
-    await send_warning(reply_to=ctx.message, title=title, sub_title='', description=description, delete_after=10)
-    await ctx.message.delete(delay=10)
+    if last_user_made_message:
+        await last_user_made_message.pin()
+    else:
+        title = 'Notice!'
+        description = 'Couldn\'t find any pinnable messages in the last 10 entries.'
+        await send_warning(reply_to=ctx.message, title=title, sub_title='', description=description, delete_after=10)
+        await ctx.message.delete(delay=10)
 
 
 @bot.command(name='clear', aliases=['erase', 'clean', 'purge'])
 async def _clear(ctx, channel : typing.Optional[discord.TextChannel], count : int = 1):
-  'Deletes past messages based on given number.'
-  if not channel:
-    channel = ctx.channel
+    'Deletes past messages based on given number.'
+    if not channel:
+        channel = ctx.channel
 
-  async with ctx.typing():
-    if channel is ctx.channel:
-      await ctx.message.delete()
-    deleted_messages = await channel.purge(limit=count)
-  
-  msg = f'Deleted {len(deleted_messages)} entries!'
-  await ctx.send(msg, delete_after=10)
+    async with ctx.typing():
+        if channel is ctx.channel:
+            await ctx.message.delete()
+        deleted_messages = await channel.purge(limit=count)
+    
+    msg = f'Deleted {len(deleted_messages)} entries!'
+    await ctx.send(msg, delete_after=10)
 
 
 if __name__ == '__main__':
-  # starts the bot
-  bot.run(os.getenv('TOKEN'))
+    # starts the bot
+    bot.run(os.getenv('TOKEN'))
